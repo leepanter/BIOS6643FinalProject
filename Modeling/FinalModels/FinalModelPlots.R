c9427f730a0784211b970a35eb0d5b2ea7db9ff8
Lee Panter <lee.panter@yahoo.com>
2019-12-01 00:18:19 -0700

    Finishing Up Final Models Script

--- Modeling/FinalModels/FinalModelPlots.R
+++ Modeling/FinalModels/FinalModelPlots.R
@@ -109,7 +109,7 @@ q
 
 
 ####	logLMwFE  ####
-loglmod.LMwFE=logLMwFEmodelFit(logmala, logcd19, subject )
+loglmod.LMwFE=logLMwFEmodelFit(mala, cd19, subject )
 (loglmod.LMwFEs=summary(loglmod.LMwFE))
 
 
@@ -200,7 +200,7 @@ q
 
 
 ####	logLMMwRE  ####
-loglmod.LMMwRE=LMMwREmodelFit(logmala, logcd19, subject )
+loglmod.LMMwRE=LMMwREmodelFit(logmala, logcd19, subject)
 (loglmod.LMMwREs=summary(loglmod.LMMwRE))
 fixef(loglmod.LMMwRE)
 ranef(loglmod.LMMwRE)
@@ -246,6 +246,359 @@ q
 
 
 
+####	POI	 ####
+lmod.POI=POImodelFit(mala, cd19, subject)
+(lmod.POIs=summary(lmod.POI))
+
+
+####	Model vs Original Data -- logLMMwRE	 ####
+range(cd19)
+xrange=seq(0,1624, length.out = 1110)
+ypredict=predict(lmod.POI, list(cd19=xrange), type = "response")
+
+plot(mala~cd19, data=dat, main="Model v Original Data")
+lines(cd19, ypredict, col=(20), lty=2, lwd=2)
+
+
+dat$fitted=fitted(lmod.POI)
+dat$resid=residuals(lmod.POI)
+
+
+####	Fitted vs Residuals -- POI	 ####
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- POI	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+####	logPOI	 ####
+loglmod.POI=logPOImodelFit(mala, cd19, subject)
+(loglmod.POIs=summary(loglmod.POI))
+
+
+####	Model vs Original Data -- logPOI	 ####
+range(logcd19)
+xrange=seq(0,7.393263, length.out = 1110)
+ypredict=predict(loglmod.POI, list(logcd19=xrange), type = "response")
+
+plot(logmala~logcd19, data=logdat, main="Model v Original Data")
+lines(logcd19, ypredict, col=(20), lty=2, lwd=2)
+
+
+####	Fitted vs Residuals -- logPOI	 ####
+dat$fitted=fitted(loglmod.POI)
+dat$resid=residuals(loglmod.POI)
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- logPOI	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+
+####	POIql	 ####
+lmod.POIql=POIqlmodelFit(mala, cd19, subject)
+(lmod.POIqls=summary(lmod.POIql))
+
+
+####	Model vs Original Data -- logLMMwRE	 ####
+range(cd19)
+xrange=seq(0,1624, length.out = 1110)
+ypredict=predict(lmod.POIql, list(cd19=xrange), type = "response")
+
+plot(mala~cd19, data=dat, main="Model v Original Data")
+lines(cd19, ypredict, col=(20), lty=2, lwd=2)
+
+####	Fitted vs Residuals -- POI	 ####
+dat$fitted=fitted(lmod.POIql)
+dat$resid=residuals(lmod.POIql)
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- POI	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+####	logPOIql	 ####
+loglmod.POIql=logPOIqlmodelFit(mala, cd19, subject)
+(loglmod.POIqls=summary(loglmod.POIql))
+
+
+####	Model vs Original Data -- logPOIql	 ####
+range(logcd19)
+xrange=seq(0,7.393263, length.out = 1110)
+ypredict=predict(loglmod.POIql, list(logcd19=xrange), type = "response")
+
+plot(logmala~logcd19, data=logdat, main="Model v Original Data")
+lines(logcd19, ypredict, col=(20), lty=2, lwd=2)
+
+####	Fitted vs Residuals -- logPOI	 ####
+dat$fitted=fitted(loglmod.POIql)
+dat$resid=residuals(loglmod.POIql)
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- logPOI	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+
+
+####	POIqlLMM	 ####
+lmod.POIqlLMM=POIqlLMMmodelFit(mala, cd19, subject)
+(lmod.POIqlLMMs=summary(lmod.POIqlLMM))
+
+
+####	Model vs Original Data -- POIqlLMM	 ####
+pred=list()
+for(i in 1:2)
+{
+  pred[[i]]=predict(lmod.POIqlLMM, level = i)
+}
+
+plot(mala~cd19, data=dat, main="Model v Original Data")
+for(i in 1:2)
+{
+  lines(cd19, pred[[i]], col=(20), lty=2, lwd=2)
+}
+
+####	Fitted vs Residuals -- POIqlLMM	 ####
+dat$fitted=fitted(lmod.POIqlLMM)
+dat$resid=residuals(lmod.POIqlLMM)
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- POIqlLMM	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+####	logPOIqlLMM	 ####
+loglmod.POIqlLMM=logPOIqlLMMmodelFit(mala, cd19, subject)
+(loglmod.POIqlLMMs=summary(loglmod.POIqlLMM))
+
+
+####	Model vs Original Data -- logPOIqlLMM	 ####
+pred=list()
+for(i in 1:2)
+{
+  pred[[i]]=predict(loglmod.POIqlLMM, level = i)
+}
+
+plot(logmala~logcd19, data=logdat, main="Model v Original Data")
+for(i in 1:2)
+{
+  lines(logcd19, pred[[i]], col=(20), lty=2, lwd=2)
+}
+
+####	Fitted vs Residuals -- logPOIqlLMM	 ####
+dat$fitted=fitted(loglmod.POIqlLMM)
+dat$resid=residuals(loglmod.POIqlLMM)
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- logPOIqlLMM	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+####	ZIP	 ####
+lmod.ZIP=mixed_model(mala~1,
+                           random = ~1|subject,
+                           data=dat,
+                           family = zi.poisson(),
+                           zi_fixed = ~ 1)
+lmod.ZIP=update(lmod.ZIP, random= ~cd19|subject)
+(lmod.ZIPs=summary(lmod.ZIP))
+
+
+####	Model vs Original Data -- ZIP	 ####
+nDF=dat
+
+plot_data=effectPlotData(lmod.ZIP, nDF)
+
+plot(plot_data$mala ~ plot_data$cd19)
+lines(plot_data$cd19, plot_data$fitted)
+
+
+####	Fitted vs Residuals -- ZIP	 ####
+dat$fitted=plot_data$fitted
+dat$resid=plot_data$resid
+
+p=ggplot(dat, aes(x=fitted, y=resid, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- ZIP	 ####
+q=ggplot(dat, aes(sample = resid))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+
+####	logZIP	 ####
+loglmod.ZIP=mixed_model(logmala~logcd19,
+                     random = ~1|subject,
+                     data=logdat,
+                     family = zi.poisson(),
+                     zi_fixed = ~ 1)
+(loglmod.ZIPs=summary(loglmod.ZIP))
+
+
+####	Model vs Original Data -- ZIP	 ####
+nDF=logdat
+
+plot_data=effectPlotData(loglmod.ZIP, nDF)
+
+plot(plot_data$logmala ~ plot_data$logcd19)
+lines(plot_data$logcd19, plot_data$fitted)
+
+
+####	Fitted vs Residuals -- ZIP	 ####
+logdat$fit=plot_data$pred
+logdat$res=plot_data$logmala-plot_data$pred
+
+p=ggplot(logdat, aes(x=fit, y=res, color=subject))+
+  geom_point()+
+  geom_hline(yintercept = 0)+
+  ggtitle("Fitted Vs Residual")+
+  xlab("Fitted Value")+
+  ylab("Residual Value")
+p
+
+
+####	Q-Q plot -- ZIP	 ####
+q=ggplot(logdat, aes(sample = res))+
+  stat_qq()+
+  stat_qq_line()
+q
+
+
+
+
+
+
+
+
+####	AIC Comparisons ####
+
+#### Non-transformed Models
+AIC(lmod.LMwFE)
+AIC(lmod.LMMwRE)
+AIC(lmod.POI)
+AIC(lmod.POIql)
+AIC(lmod.POIqlLMM)
+AIC(lmod.ZIP)
+
+
+
+#### Transformed Models
+AIC(loglmod.LMwFE)
+AIC(loglmod.LMMwRE)
+AIC(loglmod.POI)
+AIC(loglmod.POIql)
+AIC(loglmod.POIqlLMM)
+AIC(loglmod.ZIP)
+
+
+
+
+####	Summary	 Comparisons ####
+
+#### Non-transformed Models
+summary(lmod.LMwFE)
+summary(lmod.LMMwRE)
+summary(lmod.POI)
+summary(lmod.POIql)
+summary(lmod.POIqlLMM)
+summary(lmod.ZIP)
+
+
+
+#### Transformed Models
+summary(loglmod.LMwFE)
+summary(loglmod.LMMwRE)
+summary(loglmod.POI)
+summary(loglmod.POIql)
+summary(loglmod.POIqlLMM)
+summary(loglmod.ZIP)
 
 
 
